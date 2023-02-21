7fa8b3624d026426b2ebe10f84da03262f94b7ae
Stephanie Moreau <stephanie.moreau@johnabbott.qc.ca>
2023-02-21 13:56:41 -0500

    Day 1 - Intro

---
+++ Programming1/HelloWorld/src/HelloWorld.java
@@ -0,0 +1,32 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Dice rolling past 6
+		
+		
+
+		System.out.println("Hello World!"); // Hello World
+//		System.out.println("Goodbye!");
+/*		
+		System.out.print("Hello");
+		System.out.print("World");
+		
+		System.out.println();
+		System.out.println("21");
+		System.out.println(21);
+		
+		System.out.println("Steph" + "Moreau");
+		System.out.println("Steph \"teacher\" Moreau");
+	
+		System.out.println("I");
+		System.out.println("want");
+		System.out.println("new");
+		System.out.println("lines");
+		
+		System.out.println("I \nwant \nnew \nlines");
+		*/
+	}
+	// TODO clean up code
+
+}
---
+++ Programming1/Practice/src/BestMovie.java
@@ -0,0 +1,32 @@
+
+public class BestMovie {
+
+	public static void main(String[] args) {
+		
+		// Using 2 println
+		System.out.println("Shrek"); 
+		System.out.println("Mike Myers");
+		
+		System.out.println(); // extra line
+
+		// Using 1 println, 2 different ways
+		System.out.println("Shrek 2\nCameron Diaz");
+		System.out.println("Shrek 2" + "\n" + "Cameron Diaz");
+
+		System.out.println(); // extra line
+
+		// Using 2 print and 1 println
+		System.out.print("Skrek 3");
+		System.out.println();
+		System.out.print("Eddy Murphy");
+		
+		System.out.println(); // extra line
+		System.out.println(); // extra line
+		
+		// Using 3 print
+		System.out.print("Shrek 4");
+		System.out.print("\n");
+		System.out.print("Antonio Banderas");
+	}
+
+}
