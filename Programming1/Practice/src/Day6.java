8994983496c07ab5fea522013a64d03bdcb9d5a2
Stephanie Moreau <stephanie.moreau@johnabbott.qc.ca>
2023-02-28 11:36:43 -0500

    Day 6

---
+++ Programming1/Practice/src/Day6.java
@@ -0,0 +1,85 @@
+
+public class Day6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/*
+		// Example of while loop
+		int count = 0;
+		while (count < 5) {
+			System.out.print(count);
+			count++;
+			System.out.print(", ");
+		}
+		System.out.print("All Done");
+		*/
+		
+		/*
+		// Output words as many times as it has characters
+		String word = "Umbrella";
+		int countDown = word.length();
+		while(countDown-- > 0) {
+			System.out.println(countDown + ". " + word);
+		}
+		
+		int countUp = 0;
+		while(countUp < word.length()) {
+			System.out.println((countUp++) + ". " + word);
+		}
+		*/
+		
+		/*
+		// Difference between while and do-while loops
+		boolean whileLoop = false;
+		while (whileLoop) {
+			System.out.println("While" + whileLoop);
+		}
+		
+		boolean doWhileLoop = false;
+		do {
+			System.out.println("doWhile" + doWhileLoop);
+		} while (doWhileLoop);
+		*/
+		
+		/*
+		// For loop
+		int countTo = 30; //args[0];
+		int step = 7; //args[1];
+		System.out.println("I am counting to " + countTo + " with a step size of " + step + ":");
+		for (int i = 1; i <= countTo; i+=step) {
+			System.out.print(" " + i);
+		}
+		*/
+		
+		/*
+		// Multiplication Table
+		int rows = 6, cols = 5;
+		for (int r = 1; r <= rows; r++) {
+			for (int c=1; c <= cols; c++) {
+				System.out.print((r*c) + "\t");
+				System.out.print(rows);
+			}
+			System.out.println();
+		}
+		*/
+		/*
+		// Reverse a String
+		String strReverse = "Ninja Cats";
+		String reversedStr = "";
+		int strLength = strReverse.length();
+		
+		for (int i = strLength; i > 0; i-- ) {
+			 reversedStr += strReverse.charAt(i-1);
+			 System.out.println(reversedStr);
+		}
+		System.out.println(reversedStr);
+		*/
+		
+		// YOUR DEBUGGER IS YOUR BEST FRIEND!!!
+		
+			
+		
+	}
+
+}
